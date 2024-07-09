655855d4403278b9e10a59a9a56ab81a6d957161
alex <thegamerale97@gmail.com>
2024-07-09 17:12:28 +0200

    Added a simple ping command for the player

---
+++ PlayerCommands/PingUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using CommandSystem;
+using Exiled.API.Features;
+
+namespace NightMode.Commands;
+
+[CommandHandler(typeof(ClientCommandHandler))]
+public class PingUser : ICommand
+{
+    public bool Execute(ArraySegment<string> arguments, ICommandSender sender, [UnscopedRef] out string response)
+    {
+        Player player = Player.Get(sender);
+        int ping = player.Ping;
+        response = "Pong! your ping is " + ping;
+        return true;
+    }
+
+    public string Command { get; } = "ping";
+    public string[] Aliases { get; } = ["p"];
+    public string Description { get; } = "pong! returns your ping from the server";
+    public bool SanitizeResponse { get; } = true; 
+}
