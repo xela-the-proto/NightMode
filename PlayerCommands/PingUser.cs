cef14536cf71c2c8c811a905d32725f1ff030d4d
alex <thegamerale97@gmail.com>
2024-07-10 21:40:30 +0200

    More of me doing random shit

--- PlayerCommands/PingUser.cs
+++ PlayerCommands/PingUser.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
 using CommandSystem;
+using Discord;
 using Exiled.API.Features;
+using PlayerRoles.FirstPersonControl.Thirdperson;
 
 namespace NightMode.Commands;
 
 [CommandHandler(typeof(ClientCommandHandler))]
 public class PingUser : ICommand
 {
+    public string Command { get; } = "ping";
+    public string[] Aliases { get; } = ["p"];
+    public string Description { get; } = "pong! returns your ping from the server";
+    public bool SanitizeResponse { get; } = true;
+
     public bool Execute(ArraySegment<string> arguments, ICommandSender sender, [UnscopedRef] out string response)
     {
         Player player = Player.Get(sender);
@@ -15,9 +22,4 @@ public class PingUser : ICommand
         response = "Pong! your ping is " + ping;
         return true;
     }
-
-    public string Command { get; } = "ping";
-    public string[] Aliases { get; } = ["p"];
-    public string Description { get; } = "pong! returns your ping from the server";
-    public bool SanitizeResponse { get; } = true; 
 }
